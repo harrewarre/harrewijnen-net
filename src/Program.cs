c5379e848e7a925c59c5a78ec1bd4ca2c38e88ed
Sander Harrewijnen <sharrewijnen@live.com>
2021-11-26 11:30:08 +0100

    .net 6 upgrade and moved to minimal app setup
    No more startup.cs.

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,35 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Blog.Code;
 
-namespace Blog
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddSingleton<IPostResolver>(provider =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    var hostingEnvironment = provider.GetService<IWebHostEnvironment>();
+    return new PostResolver(hostingEnvironment.WebRootPath);
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.LowercaseUrls = true;
+});
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseStatusCodePagesWithRedirects("/error?code={0}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+});
+
+app.Run();
